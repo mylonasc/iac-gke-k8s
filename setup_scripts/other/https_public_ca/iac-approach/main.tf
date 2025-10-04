2e6b467296a9fa048c3fefe92b8ebf7bea0c38ff
Charilaos Mylonas <mylonas.charilaos@gmail.com>
2025-10-04 00:19:39 +0000

    added some https functionality

---
+++ setup_scripts/other/https_public_ca/iac-approach/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.11.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
+  }
+}
+
+
+
+# Install NGINX Ingress Controller
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+}
+
+# Install cert-manager
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace  = "cert-manager"
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
